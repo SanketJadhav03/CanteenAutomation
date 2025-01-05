24acbfa6b92d2cb0dda9be932cfbcb8fdecccf36
SanketJadhav03 <jadhavsanket1303@gmail.com>
2025-01-05 12:51:41 +0530

    ::::: Index php :::::

--- api/wishlist/index.php
+++ api/wishlist/index.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json");
 
 // Check if it's a GET request
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $result = $conn->query("SELECT * FROM tbl_wishlist_masters ");
+    $result = $conn->query("SELECT * FROM tbl_wishlist_masters INNER JOIN tbl_product ON tbl_product.product_id = tbl_wishlist_masters.wishlist_product_id INNER JOIN tbl_category ON tbl_product.category_id = tbl_category.category_id   ");
     $products = [];
 
     while ($row = $result->fetch_assoc()) {
