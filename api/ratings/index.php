18007045d249ae6d04ed5c1ef7661efa3a7fa402
akash <akash2005kadam>
2025-02-08 12:39:04 +0530

    Add image in Rating

--- api/ratings/index.php
+++ api/ratings/index.php
@@ -4,7 +4,12 @@ header("Content-Type: application/json");
 
 // Check if it's a GET request
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $result = $conn->query("SELECT * FROM tbl_ratings");
+    // Join ratings table with customers table to get customer name and image
+    $query = "SELECT r.*, c.customer_name, c.customer_image 
+              FROM tbl_ratings r 
+              JOIN tbl_customer c ON r.customer_id = c.customer_id";
+
+    $result = $conn->query($query);
     $products = [];
 
     while ($row = $result->fetch_assoc()) {
